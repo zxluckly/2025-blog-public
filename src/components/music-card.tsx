ce180314c165ac90c8f6ed4ae334b321a7fa48a2
zxluckly <jun15223593919@163.com>
2026-02-17 01:14:08 +0800

    修复音乐窗口无法点击
    提高z-index层级大于弹幕z-10

--- src/components/music-card.tsx
+++ src/components/music-card.tsx
@@ -166,7 +166,7 @@ export default function MusicCard() {
 
 	return (
 		<HomeDraggableLayer cardKey='musicCard' x={x} y={y} width={styles.width} height={styles.height}>
-			<Card order={styles.order} width={styles.width} height={styles.height} x={x} y={y} className={clsx('flex items-center gap-3', !isHomePage && 'fixed')}>
+			<Card order={styles.order} width={styles.width} height={styles.height} x={x} y={y} className={clsx('z-50', 'flex items-center gap-3', !isHomePage && 'fixed')}>
 				{siteContent.enableChristmas && (
 					<>
 						<img
